14e5828116c695846de1aa6aa09b7185829378a4
Christian V <viray.christianj@gmail.com>
2018-10-02 00:21:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+end 
 
-end
+  if array.length == 1
+    return array[0]}
+ 
+  elseif array.length == 2
+    return array.join(" and ")
+  
+  elseif array.length >= 3 
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end 
+end s
