56931fac41bde8803ff4dd0559075ea6fb9b935d
Christian V <viray.christianj@gmail.com>
2018-10-02 00:21:47 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 def oxford_comma(array)
-end 
 
-  if array.size == 1
+  if array.length == 1
     return array[0]}
  
-  elseif array.size == 2
+  elseif array.length == 2
     return array.join(" and ")
   
-  elseif array.size >= 3 
+  elseif array.length >= 3 
     array[-1] = "and " + array[-1]
     array.join(", ")
   end 
